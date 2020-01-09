f3e370ec0d7397ef3dd50581bb718e9cd0e0d0d4
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2020-01-09 04:25:46 +0100

    update collected consents view

--- src/pages/CollectedConsents/index.js
+++ src/pages/CollectedConsents/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
 const CollectedConsents = () => {
   return (
     <div>
-      collected consents
+    Collected Consents
+      <Grid container spacing={3}>
+        <Grid container item xs={12} spacing={3}>
+          the table
+        </Grid>
+      </Grid>
     </div>
   )
 }
