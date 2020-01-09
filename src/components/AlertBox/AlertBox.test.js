5bbb62212a8dff07370c344de8c7490b0ff8aac2
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2020-01-10 00:22:38 +0100

    add test for alert box

---
+++ src/components/AlertBox/AlertBox.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import AlertBox from './';
+
+describe('The Alert box component', () => {
+
+  const props = {
+    showAlert: true, 
+    handleCloseAlert: () => jest.fn(), 
+    type: 'success', 
+    message: 'Successfully given consent'
+  }
+
+  let wrapper = mount(<AlertBox {...props} />)
+  it('should render The Alert box correctly', () => {
+    expect(wrapper.find('Alert').length).toBe(1);
+  });
+});
